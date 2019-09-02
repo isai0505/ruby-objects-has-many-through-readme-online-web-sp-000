168ac82c3a8c0054400e8b9ded1afec7bd10de91
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-02 01:32:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -18,9 +18,9 @@ class Waiter
     end
 
     def meals
-  Meal.all.select do |meal|
-    meal.waiter == self #checking for waiter now
-  end
-end
+      Meal.all.select do |meal|
+        meal.waiter == self #checking for waiter now
+      end
+    end
 
 end
