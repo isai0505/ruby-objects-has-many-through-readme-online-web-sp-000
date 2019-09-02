7d493fe7927e42d35d55217c171a33175d4f4ba7
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-02 01:30:08 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -15,4 +15,20 @@ class Meal
   def self.all
     @@all
   end
+
+  def meals #In plain English, the customer is going to
+    # look at all of the meals, and then select
+    # only the ones that belong to them. Translated into code,
+    # that could be written like the following:
+    Meal.all.select do |meal|
+    meal.customer == self
+    end
+  end
+
+  def waiters
+    meals.map do |meal|
+    meal.waiter
+    end
+  end
+  
 end
